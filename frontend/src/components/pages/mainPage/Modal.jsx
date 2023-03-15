cebc5e24da5a38cd8252c264f0a1070f8d7ec9a6
jisoo jamie <jisoojamie@gmail.com>
2023-03-15 14:20:06 +0900

    chore: react-modal 설치

---
+++ frontend/src/components/pages/mainPage/Modal.jsx
@@ -0,0 +1,6 @@
+function Modal(props) {
+  const { message } = props;
+  return <p>{message}</p>;
+}
+
+export default Modal;
--- frontend/src/pages/mainPage.jsx
+++ frontend/src/pages/mainPage.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import mainImg from "../images/main_image.png";
+import Modal from "../components/pages/mainPage/Modal";
 
 function MainPage() {
   return (
-    <ImgWrapper>
-      <img src={mainImg} alt="메인 이미지" />
-    </ImgWrapper>
+    <>
+      <ImgWrapper>
+        <img src={mainImg} alt="메인 이미지" />
+      </ImgWrapper>
+      <div>
+        <Modal message="가정폭력 체크리스트" />
+      </div>
+    </>
   );
 }
 
