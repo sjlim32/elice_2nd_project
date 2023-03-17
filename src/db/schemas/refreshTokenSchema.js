9caf942f9d0771f42bb5be6412efef6ef543da7b
SinHT0108 <somu950108@gmail.com>
2023-03-17 17:02:22 +0900

    feat:jwtToken

---
+++ src/db/schemas/refreshTokenSchema.js
@@ -0,0 +1,20 @@
+import { Schema } from "mongoose";
+
+const refreshTokenSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    expires: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+export { refreshTokenSchema };
