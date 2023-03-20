22e43c7059e021858ece4ba3299fe08f10999809
SinHT0108 <somu950108@gmail.com>
2023-03-20 18:09:58 +0900

    refactor: user

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -4,38 +4,34 @@ import { userService } from "../services";
 const loginRequired = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
 
-  if (!token || token === "null") {
+  if (!token || token === "null")
     return res.status(401).send(`Access 토큰이 없음`);
-  }
 
   try {
-    const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    req.user = user;
+    const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     // Access Token이 만료까지 5분 이하인 경우에만 Access Token 재발급
     const currentTime = Math.floor(Date.now() / 1000);
     if (user.exp - currentTime <= 300) {
       const newAccessToken = await userService.generateAccessToken(user);
       res.setHeader("Authorization", "Bearer " + newAccessToken);
     }
+
+    req.user = user;
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError") {
       const refreshToken = req.cookies.refreshToken;
-      if (!refreshToken) {
-        return res.status(401).send(`Refresh 토큰이 없음`);
-      }
+      if (!refreshToken) return res.status(401).send(`Refresh 토큰이 없음`);
 
       try {
         const decoded = jwt.verify(
           refreshToken,
           process.env.REFRESH_TOKEN_SECRET,
         );
-
         const storedToken = await userService.getTokenByUserId(decoded.userId);
 
-        if (!storedToken) {
+        if (!storedToken)
           return res.status(401).send(`DB에 해당 id의 토큰이 없음`);
-        }
 
         if (storedToken.token !== refreshToken) {
           return res
@@ -48,19 +44,17 @@ const loginRequired = async (req, res, next) => {
 
         const newRefreshToken = await userService.generateRefreshToken(user);
         const cookieOptions = {
-          // httpOnly: true,
+          httpOnly: true,
           // secure: true,
-          // sameSite: "strict",
+          sameSite: "strict",
         };
 
         res.cookie("refreshToken", newRefreshToken, cookieOptions);
 
-        await userService.updateRefreshToken(user._id, newRefreshToken);
+        await userService.storeRefreshToken(user._id, newRefreshToken);
+
         req.user = { userId: user._id, role: user.role };
         res.setHeader("Authorization", "Bearer " + accessToken);
-        console.log(
-          "Access Token이 만료되고, Refresh 검증 후, 쿠키를 변경했습니다.",
-        );
         next();
       } catch (error) {
         return res.status(401).send(error.message);
--- src/services/userService.js
+++ src/services/userService.js
@@ -7,34 +7,34 @@ class UserService {
     this.userModel = userModel;
     this.supportUserModel = supportUserModel;
     this.refreshTokenModel = refreshTokenModel;
-    this.addUser = this.addUser.bind(this);
-    this.editUser = this.editUser.bind(this);
   }
 
   async hashPassword(password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     return hashedPassword;
   }
+
   async addUser(userInfo) {
+    const validRoles = ["user", "pending"];
+    const isValidRole = validRoles.includes(role);
+
+    if (!isValidRole) throw new Error(`유효한 userRole이 아닙니다.`);
+
     const { email, password } = userInfo;
 
     // TODO: soft Delete 적용시 수정 필요
     // isDeleted = true일 때와 false일 때
 
     const found = await this.userModel.findByEmail(email);
-    if (found) {
-      throw new Error(`이미 가입된 이메일 입니다.`);
-    }
+    if (found) throw new Error(`이미 가입된 이메일 입니다.`);
 
     const hashedPassword = await this.hashPassword(password);
     const newUserInfo = { ...userInfo, password: hashedPassword };
 
-    let createdNewUser = null;
-    if (userInfo.role === "user") {
-      createdNewUser = await this.userModel.create(newUserInfo);
-    } else {
-      createdNewUser = await this.supportUserModel.create(newUserInfo);
-    }
+    const createdNewUser =
+      userInfo.role === "user"
+        ? await this.userModel.create(newUserInfo)
+        : await this.supportUserModel.create(newUserInfo);
 
     return createdNewUser;
   }
@@ -42,37 +42,26 @@ class UserService {
   async getUserById(userId) {
     const user = await this.userModel.findById(userId);
 
-    if (!user) {
-      throw new Error(`userId가 DB에 존재하지 않습니다`);
-    }
+    if (!user) throw new Error(`userId가 DB에 존재하지 않습니다`);
 
     return user;
   }
 
   async getUsersByRole(role) {
-    const validRoles = ["user", "pending", "support"];
+    const validRoles = ["user", "support"];
     const isValidRole = validRoles.includes(role);
 
-    if (!isValidRole) {
-      throw new Error(`유효한 userRole이 아닙니다.`);
-    }
+    if (!isValidRole) throw new Error(`유효한 userRole이 아닙니다.`);
+
     const users = await this.userModel.findByRole(role);
     return users;
   }
 
-  async getTokenByUserId(userId) {
-    const user = await this.refreshTokenModel.findByUserId(userId);
-    return user;
-  }
-
   async editUser(userInfo, toUpdate) {
     const { userId, currentPassword } = userInfo;
-
     const user = await this.userModel.findById(userId);
 
-    if (!user) {
-      throw new Error(`userId가 DB에 존재하지 않습니다`);
-    }
+    if (!user) throw new Error(`userId가 DB에 존재하지 않습니다`);
 
     let newToUpdate = { ...toUpdate };
 
@@ -83,9 +72,7 @@ class UserService {
         correctPassword,
       );
 
-      if (!isPasswordCorrect) {
-        throw new Error("비밀번호가 일치하지 않습니다.");
-      }
+      if (!isPasswordCorrect) throw new Error("비밀번호가 일치하지 않습니다.");
 
       const { password, ...rest } = toUpdate;
 
@@ -95,23 +82,19 @@ class UserService {
       }
     }
 
-    let updatedUser = null;
-
-    if (user.role === "user") {
-      updatedUser = await this.userModel.updateById(userId, newToUpdate);
-    } else {
-      updatedUser = await this.supportUserModel.updateById(userId, newToUpdate);
-    }
+    const updatedUser =
+      user.role === "user"
+        ? await this.userModel.updateById(userId, newToUpdate)
+        : await this.supportUserModel.updateById(userId, newToUpdate);
 
     return updatedUser;
   }
+
   async editUserRole(userId, role) {
     const validRoles = ["support"];
     const isValidRole = validRoles.includes(role);
 
-    if (!isValidRole) {
-      throw new Error(`유효한 userRole이 아닙니다.`);
-    }
+    if (!isValidRole) throw new Error(`유효한 userRole이 아닙니다.`);
 
     const updatedUser = await this.supportUserModel.updateById(userId, {
       role,
@@ -123,20 +106,24 @@ class UserService {
   async login({ email, password }) {
     const user = await this.userModel.findByEmail(email);
 
-    if (!user) {
-      throw new Error(`Email을 찾을 수 없습니다.`);
-    }
+    if (!user) throw new Error(`Email을 찾을 수 없습니다.`);
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordCorrect) {
-      throw new Error("패스워드가 일치하지 않습니다.");
-    }
+    if (!isPasswordCorrect) throw new Error("패스워드가 일치하지 않습니다.");
+
     const accessToken = await this.generateAccessToken(user);
     const refreshToken = await this.generateRefreshToken(user);
-    await this.createRefreshToken(user.id, refreshToken);
+    await this.storeRefreshToken(user.id, refreshToken);
+
     return { accessToken, refreshToken };
   }
+
+  async getTokenByUserId(userId) {
+    const user = await this.refreshTokenModel.findByUserId(userId);
+    return user;
+  }
+
   async generateAccessToken(user) {
     const accessToken = jwt.sign(
       { userId: user._id, role: user.role },
@@ -156,30 +143,17 @@ class UserService {
 
     return refreshToken;
   }
-  async createRefreshToken(userId, token) {
-    const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-    const found = await this.refreshTokenModel.findByUserId(userId);
 
-    if (!found) {
-      await this.refreshTokenModel.create({
-        userId,
-        token: token,
-        expires,
-      });
-    } else {
-      await this.refreshTokenModel.updateByUserId(userId, {
-        token: token,
-        expires,
-      });
-    }
-  }
-  async updateRefreshToken(userId, token) {
-    const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  async storeRefreshToken(userId, token) {
+    const AFTER_WEEK = Date.now() + 7 * 24 * 60 * 60 * 1000;
+    const expires = new Date(AFTER_WEEK);
+
     await this.refreshTokenModel.updateByUserId(userId, {
       token: token,
       expires,
     });
   }
+
   async deleteRefreshToken(userId) {
     await this.refreshTokenModel.deleteByUserId(userId);
   }
@@ -187,9 +161,8 @@ class UserService {
   async deleteUser(userId) {
     const user = await this.userModel.deleteById(userId);
 
-    if (!user) {
-      throw new Error(`가입된 정보가 없습니다.`);
-    }
+    if (!user) throw new Error(`가입된 정보가 없습니다.`);
+
     await this.refreshTokenModel.deleteByUserId(userId);
     return user;
   }
@@ -200,4 +173,5 @@ const userService = new UserService(
   supportUserModel,
   refreshTokenModel,
 );
+
 export { userService, UserService };
