0ca69611063d4363d70d246ddfe2c18d05ce88f8
cookooyas <cookooyas@gmail.com>
2023-03-22 19:18:14 +0900

    feature/chatAPI : chat API merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,3 +2,4 @@ export * from "./userRouter";
 export * from "./categoryRouter";
 export * from "./postRouter";
 export * from "./replyRouter";
+export * from "./chatRouter";
--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,4 @@ export * from "./userService";
 export * from "./categoryService";
 export * from "./postService";
 export * from "./replyService";
+export * from "./chatService";
