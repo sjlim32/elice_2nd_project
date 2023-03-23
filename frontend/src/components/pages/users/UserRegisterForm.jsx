fb43ae519315e6c3b05b235b4d76a864ade537e8
jsm00929 <jsm00929@naver.com>
2023-03-23 20:05:32 +0900

    fix:merge conflict

---
+++ frontend/src/components/pages/users/UserRegisterForm.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import * as API from "../../../utils/api";
+import { useNavigate } from "react-router-dom";
+
+function UserRegisterForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const validateEmail = () => {
+    const emailForm = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    if (emailForm.test(email) == false) {
+      setError("invalide Email Address");
+      return false;
+    }
+    return true;
+  };
+
+  const validatePassword = () => {
+    if (password !== confirmPassword) {
+      setError("password is not confirmed");
+      return false;
+    }
+    return true;
+  };
+
+  const validateForm = () => {
+    return validateEmail() && validatePassword();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userData = {
+      role: "user",
+      email,
+      password,
+    };
+    const validateResult = validateForm();
+    if (validateResult) {
+      try {
+        const res = await API.post("/users", userData);
+        console.log(res.data);
+        navigate("/");
+      } catch (err) {
+        alert(error);
+      }
+    } else alert(error);
+  };
+
+  return (
+    <div>
+      <input
+        id="email"
+        value={email}
+        placeholder="abc@abc.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button id="checkEmail">중복 확인</button>
+
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <input
+        id="confirmPassword"
+        type="password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+
+      <button id="submit" onClick={handleSubmit}>
+        가입하기
+      </button>
+    </div>
+  );
+}
+
+export default UserRegisterForm;
